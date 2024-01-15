bf295133a5aac35d65f82966906ddd47e7de8ff7
yasser farhadi <yasser.farhadee@gmail.com>
2024-01-15 20:52:58 +0330

    local search for home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,5 +1,28 @@
+import LocalSearch from '@/components/shared/search/LocalSearch';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
 
 export default function Home() {
-  return <div className='h-screen'>salam</div>;
+  return (
+    <>
+      <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
+        <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+        <Link href='/ask-question' className='flex justify-end max-sm:w-full'>
+          <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900 max-sm:w-full'>
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center'>
+        <LocalSearch
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholcer='Search for Questions'
+          className='flex-1'
+        />
+      </div>
+    </>
+  );
 }
