8d041abcd8d5ab9e7c40df05cfb8678dcaeb4b19
yasser farhadi <yasser.farhadee@gmail.com>
2024-01-18 21:19:26 +0330

    tag input

--- src/components/forms/Question.tsx
+++ src/components/forms/Question.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { ControllerRenderProps, useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
@@ -17,10 +18,11 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { QuestionsSchema } from '@/lib/validations';
+import { Badge } from '@/components/ui/badge';
 
 function Question() {
-  const editorRef = React.useRef(null);
-  // 1. Define your form.
+  // const editorRef = React.useRef(null);
+
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
@@ -30,12 +32,46 @@ function Question() {
     },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+  function handleInputKeyDown(
+    event: React.KeyboardEvent<HTMLInputElement>,
+    field: ControllerRenderProps<typeof form, 'tags'>,
+  ) {
+    if (event.key === 'Enter' && field.name === 'tags') {
+      event.preventDefault();
+      const tagInput = event.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+      if (tagValue !== '') {
+        if (tagValue.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag must be less than 15 characters.',
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue('tags', [...field.value, tagValue]);
+          tagInput.value = '';
+          form.clearErrors('tags');
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  }
+
+  function handleBadgeRemove(
+    tag: string,
+    field: ControllerRenderProps<typeof form>,
+  ) {
+    const newTags = field.value.filter((t) => t !== tag);
+    form.setValue('tags', newTags);
+  }
+
   return (
     <Form {...form}>
       <form
@@ -111,11 +147,33 @@ function Question() {
                 Tags <span className='text-primary-500'>*</span>
               </FormLabel>
               <FormControl className='mt-3.5'>
-                <Input
-                  className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
-                  {...field}
-                  placeholder='Add tags...'
-                />
+                <>
+                  <Input
+                    className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                    placeholder='Add tags...'
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className='flex-start mt-2.5 gap-2.5'>
+                      {field.value.map((tag) => (
+                        <Badge
+                          key={tag}
+                          className='subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize'
+                        >
+                          {tag}
+                          <Image
+                            src='/assets/icons/close.svg'
+                            alt='Close icon'
+                            width={12}
+                            height={12}
+                            className='cursor-pointer object-contain invert-0 dark:invert'
+                            onClick={() => handleBadgeRemove(tag, field)}
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className='body-regular mt-2.5 text-light-500'>
                 Add up to 3 tags to describe what your question is about. You
