d164cc50e4e9fcfc3663177f854e390148647c3a
yasser farhadi <yasser.farhadee@gmail.com>
2024-01-28 20:14:57 +0330

    home page search implementation

--- src/lib/actions/question.action.ts
+++ src/lib/actions/question.action.ts
@@ -16,11 +16,28 @@ import { revalidatePath } from 'next/cache';
 import { notFound } from 'next/navigation';
 import Answer from '@/database/answer.model';
 import Interaction from '@/database/interaction.model';
+import { FilterQuery } from 'mongoose';
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
     await connectToDatabase();
-    const questions = await Question.find({})
+
+    const { searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        {
+          title: { $regex: new RegExp(searchQuery, 'i') },
+        },
+        {
+          content: { $regex: new RegExp(searchQuery, 'i') },
+        },
+      ];
+    }
+
+    const questions = await Question.find(query)
       .populate({ path: 'tags', model: Tag })
       .populate({ path: 'author', model: User })
       .sort({ createdAt: -1 });
