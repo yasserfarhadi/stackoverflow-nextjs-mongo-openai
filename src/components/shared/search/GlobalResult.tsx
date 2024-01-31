bb33837110849b1784232655960d1dacc6cb66db
yasser farhadi <yasser.farhadee@gmail.com>
2024-01-31 04:54:10 +0330

    implement filter tags in global result component

--- src/components/shared/search/GlobalResult.tsx
+++ src/components/shared/search/GlobalResult.tsx
@@ -5,6 +5,7 @@ import { ReloadIcon } from '@radix-ui/react-icons';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
+import GlobalFilters from './GlobalFilters';
 
 const createLink = (
   type: 'question' | 'answer' | 'user' | 'tag',
@@ -57,7 +58,7 @@ const GlobalResult = () => {
 
   return (
     <div className='absolute inset-x-0 top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400'>
-      <p className='text-dark400_light900 paragraph-semibold px-5'>Filters</p>
+      <GlobalFilters />
       <div className='my-5 h-[1px] bg-light-700/50 dark:bg-dark-500/50'></div>
       <div className='space-y-5'>
         <p className='text-dark400_light900 paragraph-semibold px-5'>
