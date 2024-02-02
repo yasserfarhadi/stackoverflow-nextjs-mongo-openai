b4bc7251bf38f939e538ed0b41a3321655ba63d0
yasser farhadi <yasser.farhadee@gmail.com>
2024-02-02 04:08:44 +0330

    add metadata to homepage

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -11,6 +11,13 @@ import { SearchParamsProps } from '@/types';
 import Link from 'next/link';
 import React from 'react';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home / BBK Overflow',
+  description: 'BBK Overflow is a community of 1,600,000+ developers. Join us.',
+};
+
 export default async function Home({
   searchParams: { q, filter, page },
 }: SearchParamsProps) {
